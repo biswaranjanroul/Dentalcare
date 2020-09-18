e47904a76c3cc93e05791e16c4c8406f89279b2c
Biswaranjan Roul <biswaranjanroul2@gmail.com>
2020-09-18 10:27:40 +0530

    Send email change in settings.py

--- Dentalpro/settings.py
+++ Dentalpro/settings.py
@@ -123,13 +123,13 @@ STATICFILES_DIRS = [
 ]
 
 
-#Mail sending 
-EMAIL_HOST = "smtp.gmail.com"  # or "smpt.google.com"
-EMAIL_PORT = 587
-EMAIL_HOST_USER ='Youremail@gmail.com'
-EMAIL_HOST_PASSWORD ="Your email Password"
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+#Mail sending through gmail account 
+# EMAIL_HOST = "smtp.gmail.com"  # or "smpt.google.com"
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER ='Youremail@gmail.com'
+# EMAIL_HOST_PASSWORD ="Your email Password"
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
 
 
 
@@ -139,11 +139,11 @@ EMAIL_USE_SSL = False
 #This the comand for Start the Fake Message server
 # python -m smtpd -n -c DebuggingServer localhost:1025
 
-# EMAIL_HOST = 'localhost'
-# EMAIL_PORT = 1025
-# EMAIL_HOST_USER =''
-# EMAIL_HOST_PASSWORD = ''
-# EMAIL_USE_TLS = False
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+EMAIL_HOST_USER =''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
 
 
 
